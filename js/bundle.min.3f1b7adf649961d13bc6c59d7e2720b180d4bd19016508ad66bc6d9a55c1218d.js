04fb9ff30a9f27826b7cddd4aa60597afcb4b21f
Matthías Ragnarsson <matti@netveldi.com>
2024-09-22 00:01:40 +0000

    .

--- js/bundle.min.3f1b7adf649961d13bc6c59d7e2720b180d4bd19016508ad66bc6d9a55c1218d.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{const e=Array.prototype.slice.call(document.querySelectorAll(".navbar-burger"),0);e.length>0&&e.forEach(e=>{e.addEventListener("click",()=>{const t=e.dataset.target,n=document.getElementById(t);e.classList.toggle("is-active"),n.classList.toggle("is-active")})})}),document.addEventListener("DOMContentLoaded",()=>{})
